df82883414a96938fa3569ac098df31a311187e9
Jeonsw <fpdlsnsqlc3@naver.com>
2024-05-13 09:20:16 +0900

    변수명 변경(엔진에서 _ 사용불가)

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main()
 {
-	int _Number1 = 0;
-	int _Number2 = 0;
+	int Number1 = 0;
+	int Number2 = 0;
 
-	cin >> _Number1 >> _Number2;
-	cout << _Number1 + _Number2 << endl;
-	cout << _Number1 - _Number2 << endl;
-	cout << _Number1 * _Number2 << endl;
-	cout << _Number1 / _Number2 << endl;
+	cin >> Number1 >> Number2;
+	cout << Number1 + Number2 << endl;
+	cout << Number1 - Number2 << endl;
+	cout << Number1 * Number2 << endl;
+	cout << Number1 / Number2 << endl;
 
 	return 0;
 }
