763a8bf767506220f143856c0ef16cad5d063b6f
Jeonsw <fpdlsnsqlc3@naver.com>
2024-05-13 09:15:49 +0900

    저장소 초기화

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int _Number1 = 0;
+	int _Number2 = 0;
+
+	cin >> _Number1 >> _Number2;
+	cout << _Number1 + _Number2 << endl;
+	cout << _Number1 - _Number2 << endl;
+	cout << _Number1 * _Number2 << endl;
+	cout << _Number1 / _Number2 << endl;
+
+	return 0;
+}
