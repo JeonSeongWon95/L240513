c6ded5ded13803aef2c5d5a387c17a44bf55c9a2
Jeonsw <fpdlsnsqlc3@naver.com>
2024-05-13 09:26:15 +0900

    안내 문구 추가

--- main.cpp
+++ main.cpp
@@ -8,10 +8,11 @@ int main()
 	int Number2 = 0;
 
 	cin >> Number1 >> Number2;
-	cout << Number1 + Number2 << endl;
-	cout << Number1 - Number2 << endl;
-	cout << Number1 * Number2 << endl;
-	cout << Number1 / Number2 << endl;
+	cout << Number1 << " + " << Number2 << " = " << Number1 + Number2 << endl;
+	cout << Number1 << " - " << Number2 << " = " << Number1 - Number2 << endl;
+	cout << Number1 << " * " << Number2 << " = " << Number1 * Number2 << endl;
+	cout << Number1 << " / " << Number2 << " = " << Number1 / Number2 << endl;
+
 	cout << "연산이 완료되었습니다." << endl;
 
 	return 0;
